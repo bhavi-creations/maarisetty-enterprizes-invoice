1865e33afc34ac8d58fa6c2379c40efa4253f6bf
Rajkumargiduthuri <rajkumar16371@gmail.com>
2024-02-10 16:15:08 +0530

    database credentials changed

--- bhavidb.php
+++ bhavidb.php
@@ -8,9 +8,9 @@
         $database = 'me_invoice_generator';
     } else {
         // Server environment details
-        $username = 'cnpthbbs_invoice_user1';
-        $pass = '%tNc6peV4-}w';
-        $database = 'cnpthbbs_invoice1';
+        $username = 'bhavi@maarisetty';
+        $pass = 'Bhavi@client';
+        $database = 'maarisetty';
     }
 
     $conn = mysqli_connect($server,$username,$pass,$database);
--- edit_quotation.php
+++ edit_quotation.php
@@ -16,21 +16,7 @@ require_once('bhavidb.php');
 
 function getInvoiceId()
 {
-    $server = 'localhost';
-    // Condition to check if the script is running locally or on a server
-    if ($_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST'] == '127.0.0.1') {
-        // Local environment details
-        $username = 'root';
-        $pass = '';
-        $database = 'me_invoice_generator';
-    } else {
-        // Server environment details
-        $username = 'cnpthbbs_invoice_user1';
-        $pass = '%tNc6peV4-}w';
-        $database = 'cnpthbbs_invoice1';
-    }
-
-    $conn = mysqli_connect($server, $username, $pass, $database);
+   include('bhavidb.php');
 
     if ($conn->connect_error) {
         die('Error : (' . $conn->connect_errno . ') ' . $conn->connect_error);
